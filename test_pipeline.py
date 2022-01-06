36df376a96e2ad8ef0c01fc233f5b2641e30ff7b
ederson <ederson.andre@atos.net>
2022-01-05 23:16:57 -0300

    fix test_pipeline

--- test_pipeline.py
+++ test_pipeline.py
@@ -8,23 +8,29 @@ from pipeline.ml.data import load_data
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-PATH = "data/census.csv"
+
 
 @pytest.fixture
-def data(PATH):
+def data():
     """ 
     Load data for testing.
     """
-
+    PATH = "data/census.csv"
+    
     assert os.path.exists(PATH), f"Path doesn't exist. Please, verify."
 
     df = load_data(PATH)
 
-    assert df is not None, "No data was loaded."
-    assert df.shape[0] > 0 , "Testing import_data: The file doesn't appear to have rows."
-    assert df.shape[1] > 0, "Testing import_data: The file doesn't appear to have columns."
-
     return df
+
+
+def test_df(data):
+
+    assert data is not None, "No data was loaded."
+    assert data.shape[0] > 0 , "Testing import_data: The file doesn't appear to have rows."
+    assert data.shape[1] > 0, "Testing import_data: The file doesn't appear to have columns."
+
+
 
     
 
